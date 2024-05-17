09a6903402e0cb7c01d767c603faade1a6517eb7
Hans How <hanshow00@gmail.com>
2024-05-16 20:45:04 -0500

    arrow lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define the divide function
+function divide(){
+    return 2000/100;
+}
+
+// Define the square arrow function
+const square = (num) => num * num;
+
+// Define the add arrow function
+const add = (num1, num2) => num1 + num2;
